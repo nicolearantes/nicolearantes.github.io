3045bf8c4cfc7734d5e217d2fe9ad0a275f99d07
nicole.arantes <nicolearantes@live.com>
2023-10-17 19:32:15 +0200

    Building the home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,49 +1,73 @@
 import React from "react"
 import style from "./Home.module.scss"
-import { SlSocialLinkedin } from "react-icons/sl"
-import { SlSocialGithub } from "react-icons/sl"
-import { IconContext } from "react-icons"
+import {IconContext} from "react-icons"
+import {FaGithubAlt, FaLinkedin} from "react-icons/fa";
+import imageRight from "../static/firstEvent.jpg"
+import imageLeft from "../static/firstInteraction.jpg"
+import classNames from "classnames";
 
 const Home: React.FC = () => {
-    return <div >
-        <div className={style.homewrapper}>
-            <img className={style.firstart} src="../nicolearantes.png" alt="girl in front of a computer doing a surprised face"></img>
-            <div className={style.titleText}>
-                <h1 className={style.title}>Nicole Arantes</h1>
-                <h3 className={style.subtitle}>Software Engineer </h3>
-                <div className={style.socialMedia}>
-                    <IconContext.Provider value={{ className: style.icons }}>
-                    <SlSocialLinkedin />
-                    <SlSocialGithub />
+    return (
+        <div className={style.homeWrapper}>
+            <div className={style.introWrapper}>
+                <div className={style.textWrapper}>
+                    <div className={style.text}>
+                        <h2 className={style.title}>M</h2>
+
+                        <p className={style.description}>My name is Nicole Arantes.
+                            I work as a Software Engineer with focus on
+                            web development using React,
+                            Typescript among other technologies.
+                            I also love art, sports and fashion.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className={classNames("row", style.imgWrapper)}>
+                <img className={classNames("col-xs-12 col-sm-6", style.introImg)} src={imageRight}
+                     alt="myself wearing a black dress in an empty stagein front of some red letters written we are devs"/>
+                <img className={classNames("col-xs-12 col-sm-6", style.introImg)} src={imageLeft}
+                     alt="group of woman wearing an orange t-shite written dajngo girls"/>
+            </div>
+
+            <div className={style.contactWrapper}>
+                <h2 className={style.contactTitle}>HEY, I JUST MET YOU</h2>
+                <div className={style.contactText}>
+                    <p>But please feel free to reach me out through my LinkedIn specially if you are a beginner and have
+                        questions.
+                        <p>I know it can be scary sometimes.</p>
+                        And if you curious you can take a look into my Github where I have also some old and ugly code,
+                        to
+                        see how much I developed since I started and maybe that can help you too.
+                    </p>
+                    <p>Because in the end to be a developer is all about a constant process of evolution. You got
+                        this!</p>
+                    <p> With love,</p>
+                    <p>Nicole Arantes</p>
+                </div>
+            </div>
+
+            <div className={style.footer}>
+                <h6>This site was made with ♥ by Nicole Arantes</h6>
+                <div className={style.iconsWrapper}>
+                    <IconContext.Provider value={{size: "1.5em", className: style.socialMedia}}>
+                        <a href="https://www.linkedin.com/in/nicolearantes/" target="_blank"
+                           rel="noreferrer">
+                            <FaLinkedin title="icon of Linkedin"/>
+                        </a>
+                    </IconContext.Provider>
+
+                    <IconContext.Provider value={{size: "1.5em", className: style.socialMedia}}>
+                        <a href="https://github.com/nicolearantes" target="_blank" rel="noreferrer">
+                            <FaGithubAlt title="icon of Github"/>
+                        </a>
                     </IconContext.Provider>
                 </div>
             </div>
         </div>
-        <div className={style.introduction}>
-            <div className={style.video}></div>
-            <div className={style.textWrapper}>
-                <h2>I'm Nic,</h2>
-                <p>a Brazilian Software Engineer based in Berlin.
-                    I've been working as a Software Engineer for about 2 years with focus on
-                    web development using React,
-                    Typescript among other technologies.
-                </p>
-                <div className={style.imagesWrapper}>
-                    <div className={style.introductionPictures}></div>
-                    <div className={style.introductionPictures}></div>
-                    <div className={style.introductionPictures}></div>
-                </div>
-            </div>
-        </div>
-        <div className={style.contactWrapper}>
-            <h2>HEY,I JUST MET YOU</h2>
-            <p>Please feel free to reach me through my LinkedIn
-                either if you are a recruiter or if you just started to study programming and have questions, I try to reply to all messages.</p>
-        </div>
-        <div className={style.footer}>
-            <h6>Everything in this site was made with HEART by Nicole Arantes</h6>
-        </div>
-    </div>
+    )
 }
 
 export default Home
