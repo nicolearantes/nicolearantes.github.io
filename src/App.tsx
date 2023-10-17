47e33525613c790bc9c6127ea47e378ad43d091e
nicole.arantes <nicolearantes@live.com>
2023-10-17 19:32:55 +0200

    Add some App style and index bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import style from './App.module.scss';
 import Home from './pages/Home/Home';
+import React from "react";
+import headerWeb from "./pages/static/header-web.png";
+import headerMobile from "./pages/static/header-mobile.png"
+import {isMobile} from "./utils/dimensions";
 
 function App(): React.ReactElement {
-  return (
-    <div className={style.pageWrapper}>
-      <Home />
-    </div>
-  );
+
+
+    return (
+        <div>
+            <img className={style.homeHeader} src={isMobile ? headerMobile : headerWeb}
+                 alt="girl in front of a computer doing a surprised face"></img>
+
+            <div className={style.pageWrapper}>
+                <Home/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
