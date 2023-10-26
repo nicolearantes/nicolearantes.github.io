b41af40351265b9244de75396a58369b477c28ca
nicole.arantes <nicolearantes@live.com>
2023-10-26 07:39:54 +0200

    Fix header

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import style from './App.module.scss';
 import Home from './pages/Home/Home';
 import React from "react";
 import headerWeb from "./pages/static/header-web.png";
-import headerMobile from "./pages/static/header-mobile.png"
-import {isMobile} from "./utils/dimensions";
 
 function App(): React.ReactElement {
 
 
     return (
         <div>
-            <img className={style.homeHeader} src={isMobile ? headerMobile : headerWeb}
+            <img className={style.homeHeader} src={headerWeb}
                  alt="girl in front of a computer doing a surprised face"></img>
 
             <div className={style.pageWrapper}>
