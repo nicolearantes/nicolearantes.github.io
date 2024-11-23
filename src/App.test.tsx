4f57152dde73df7cf6dc9d63190b3dd361d80c5e
nicole.arantes <nicolearantes@live.com>
2024-11-23 15:34:13 +0100

    Delete react and code related to it

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Everything in this site was made with HEART by Nicole Arantes/i);
-  expect(linkElement).toBeInTheDocument();
-});
