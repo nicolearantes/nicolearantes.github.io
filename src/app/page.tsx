d3c391379782b4da0f8f9996184872da38486c41
nicole.arantes <nicolearantes@live.com>
2024-11-23 16:56:01 +0100

    Remoce not used style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import styles from "./page.module.css";
+import cn from "classnames";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={cn(styles.page, 'text-3xl font-bold underline')}>
       <p>Under Construction</p>
     </div>
   );
